6cf29efa0c501e0d4fc9e6eda8f8fa2e31aab379
Kraka Kristianto <krakakristianto@gmail.com>
2026-04-20 21:11:46 +0700

    TugasP7

--- Pertemuan7/tugasP7/src/TugasP7B.java
+++ Pertemuan7/tugasP7/src/TugasP7B.java
@@ -4,27 +4,42 @@
 // Tugas switch
 // menambahkan pilihan menu
 
-// Nama : Kraka Kristianto
-// Nim : 241011401389
-// Tanggal : 14 April 2026, Pertemuan 7 (offline) minggu non elearning
-
 import java.util.Scanner;
 public class TugasP7B {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String hewan = "";
-        System.out.print("Masukkan huruf (a/b): ");
+        System.out.println("index hewan (free version)");
+        System.out.print("Masukkan huruf (a sampai h): ");
         String huruf = input.nextLine();
 
         switch (huruf) {
             case "a":
-                hewan = "ayam";              
+                hewan = "ayam, anjing, angsa";              
                 break;
             case "b":
-                hewan = "burung";
+                hewan = "burung, bebek, badak";
+                break;
+            case "c":
+                hewan = "cicak, capung, cacing";
+                break;
+            case "d":
+                hewan = "domba, dinosaurus";
+                break;
+            case "e":
+                hewan = "elang, entok, emu";
+                break;
+            case "f":
+                hewan = "flamingo, ferret, falcon";
+                break;
+            case "g":
+                hewan = "gajah, gorila, gagak";
+                break;
+            case "h":
+                hewan = "Harimau, hiu, hamster";
                 break;
             default:
-                hewan = "hewan tidak diketahui";
+                hewan = "hewan belum diketahui ";
                 break;
         }
         System.out.println("hewan yang dipilih: " + hewan);
