0598b1dc94d7961ff794377c0921bc9b772ea915
Kraka Kristianto <krakakristianto@gmail.com>
2026-04-20 21:24:48 +0700

    TugasP7Switch

--- Pertemuan7/tugasP7/src/TugasP7B.java
+++ Pertemuan7/tugasP7/src/TugasP7B.java
@@ -38,6 +38,60 @@ public class TugasP7B {
             case "h":
                 hewan = "Harimau, hiu, hamster";
                 break;
+            case "i":
+                hewan = "ikan, itik, iguana";
+                break;
+            case "j":
+                hewan = "jerapah, jangkrik, jaguar";
+                break;
+            case "k":
+                hewan = "kucing, kuda, kambing";
+                break;
+            case "l":
+                hewan = "lebah, landak, laler";
+                break;
+            case "m":
+                hewan = "monyet, macan, musang";
+                breka;
+            case "n":
+                hewan = "nyamuk, nila"
+                break;
+            case "o":
+                hewan = "orca, orangutan"
+                break;
+            case "p":
+                hewan = "panda, paus, penyu";
+                break;
+            case "q":
+                hewan = "indeks huruf ini tidak diketahui"
+                break;
+            case "r":
+                hewan = "rakun, rusa, rubah";
+                break;
+            case "s":
+                hewan = "sapi, semut, singa";
+                break;
+            case "t":
+                hewan = "tapir, tikus, tupai";
+                break;
+            case "u":
+                hewan = "ular, unta, udang";
+                break;
+            case "v":
+                hewan = "indeks huruf ini tidak dikteahui";
+                break;
+            case "w":
+                hewan = "walang sangit, wombat";
+                break;
+            case "x":
+                hewan = "indeks dihuruf ini tidak diketahui";
+                break;
+            case "y":
+                hewan = "yak";
+                break;
+            case "z":
+                hewan = "zebra";
+                break;
             default:
                 hewan = "hewan belum diketahui ";
                 break;
