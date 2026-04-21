12b511fb20c7bc4f7c8ee611afd1efe54518e350
Kraka Kristianto <krakakristianto@gmail.com>
2026-04-21 09:57:35 +0700

    Latihan8

--- Pertemuan8/src/Latihan8.java
+++ Pertemuan8/src/Latihan8.java
@@ -1,5 +1,48 @@
-public class App {
+// Nama : Kraka Kristianto
+// Nim : 241011401389
+// Tanggal : 21 April 2026, Pertemuan 8 (offline) minggu elearning
+// materi minggu ini membahas perulangan
+// for, while, do-while.
+// 1. struktur for
+// for (inisiasi; kondisi; incirement) {
+//     statement for
+// }
+// 2. struktur while
+// while (kondisi) {
+//   statement while 
+// }
+// 3. Struktur do-while
+// do {
+//    statement
+// } while (kondisi)
+//
+// for (a=1;a<=b;a++) {
+//  System.out.println("nilai: " +a);
+//  }
+//  
+
+
+
+
+
+
+public class Latihan8 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        for (int a=1; a<=5; a+++){
+            System.out.println("nilai for: " +a);
+        }
+        System.out.println("\n");
+
+        int b=1;
+        while (b<=5){
+            System.out.println("nilai while: " +b); b++;
+        }
+        System.out.println("\n");
+
+        int c=1;
+        do {
+            System.out.println("nilai do-while: " +c); c++;
+        } while (c<=5);
     }
 }
+
